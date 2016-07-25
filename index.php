d4a94a5e508c3b95c7cb5c2467ffdd11df4867bd
Alexey Kobyleha <uazonner@gmail.com>
2016-07-26 02:12:46 +0300

    Add task #3,4

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8">
 <?php
 echo "<h3>Task 1</h3>";
 /*
@@ -33,4 +34,52 @@ for ($i=0; $i <= strlen($randomNumber); $i++) {
 
 echo $result;
 
+echo "<h3>Task 3</h3>";
+
+/*
+echo 'Разработайте программу, которая из чисел 20 .. 45 находила те, которые делятся на 5 и найдите сумму этих чисел. Рекомендую использовать функцию fmod для определения "делится число" или "не делится".'
+*/
+
+$start = 20;
+$end = 45;
+$divider = 5;
+$result = 0;
+
+for ($i=$start; $i <= $end; $i++) { 
+	if (fmod($i, $divider) == 0) {
+		$result += $i;
+	}
+}
+
+echo $result;
+
+echo "<h3>Task 4</h3>";
+
+/*
+Ваше задание создать массив, наполнить его случайными значениями (функция rand), найти максимальное и минимальное значение и поменять их местами.
+*/ 
+
+$qlArray = 45;
+
+for ($i=0; $i <= $qlArray; $i++) { 
+	$random[] = rand(0,100);
+}
+
+$min = min($random);
+$max = max($random);
+
+
+$keyMin = array_search(min($random), $random);
+$keyMax = array_search(max($random), $random);
+
+echo "Ключ минимального числа: " . $keyMin . " = Значению " . $min . "<br>";
+echo "Ключ максимального числа: " . $keyMax . " = Значению " . $max . "<br>";
+
+$random[$keyMin] = $max;
+$random[$keyMax] = $min;
+
+echo "Теперь ключ: " . $keyMin . " = Значению " . $random[$keyMin] . "<br>";
+echo "Теперь ключ: " . $keyMax . " = Значению " . $random[$keyMax] . "<br>";
+
+echo "<h3>Task 5</h3>";
 ?>
